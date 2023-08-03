10bdc5e82a92f132906e0de2b591bf5a12471f59
HoangDSR <nvh.02021995@gmail.com>
2023-08-03 17:22:48 +0700

    Done PHIẾU CHUYỂN ĐƠN TỐ CÁO

---
+++ QuanLyDAPM/GQKN-BBKT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyDAPM
+{
+    public partial class GQKN_BBKT : Form
+    {
+        public GQKN_BBKT()
+        {
+            InitializeComponent();
+        }
+    }
+}
