d51272acac2edcfa271531a95aad830640206b7e
HoangDSR <nvh.02021995@gmail.com>
2023-08-10 19:55:57 +0700

    Done

---
+++ QuanLyDAPM/HST-KHBVMT.Designer.cs
@@ -0,0 +1,530 @@
+﻿namespace QuanLyDAPM;
+
+partial class HST_KHBVMT
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HST_KHBVMT));
+        label21 = new Label();
+        label22 = new Label();
+        label23 = new Label();
+        textBox8 = new TextBox();
+        textBox9 = new TextBox();
+        textBox10 = new TextBox();
+        textBox11 = new TextBox();
+        textBox2 = new TextBox();
+        label1 = new Label();
+        label12 = new Label();
+        label11 = new Label();
+        textBox1 = new TextBox();
+        label2 = new Label();
+        textBox3 = new TextBox();
+        textBox4 = new TextBox();
+        label3 = new Label();
+        textBox16 = new TextBox();
+        textBox15 = new TextBox();
+        label19 = new Label();
+        label18 = new Label();
+        textBox14 = new TextBox();
+        label17 = new Label();
+        textBox13 = new TextBox();
+        label5 = new Label();
+        label16 = new Label();
+        textBox5 = new TextBox();
+        label7 = new Label();
+        textBox6 = new TextBox();
+        label4 = new Label();
+        label20 = new Label();
+        textBox7 = new TextBox();
+        label6 = new Label();
+        label10 = new Label();
+        textBox12 = new TextBox();
+        label9 = new Label();
+        label8 = new Label();
+        richTextBox1 = new RichTextBox();
+        button2 = new Button();
+        button1 = new Button();
+        label43 = new Label();
+        label42 = new Label();
+        label46 = new Label();
+        SuspendLayout();
+        // 
+        // label21
+        // 
+        label21.AutoSize = true;
+        label21.Location = new Point(825, 93);
+        label21.Name = "label21";
+        label21.Size = new Size(45, 20);
+        label21.TabIndex = 98;
+        label21.Text = ", năm";
+        // 
+        // label22
+        // 
+        label22.AutoSize = true;
+        label22.Location = new Point(723, 93);
+        label22.Name = "label22";
+        label22.Size = new Size(54, 20);
+        label22.TabIndex = 97;
+        label22.Text = ", tháng";
+        // 
+        // label23
+        // 
+        label23.AutoSize = true;
+        label23.Location = new Point(619, 93);
+        label23.Name = "label23";
+        label23.Size = new Size(48, 20);
+        label23.TabIndex = 96;
+        label23.Text = ", ngày";
+        // 
+        // textBox8
+        // 
+        textBox8.Location = new Point(879, 90);
+        textBox8.Name = "textBox8";
+        textBox8.Size = new Size(55, 27);
+        textBox8.TabIndex = 95;
+        // 
+        // textBox9
+        // 
+        textBox9.Location = new Point(778, 89);
+        textBox9.Name = "textBox9";
+        textBox9.Size = new Size(39, 27);
+        textBox9.TabIndex = 94;
+        // 
+        // textBox10
+        // 
+        textBox10.Location = new Point(676, 89);
+        textBox10.Name = "textBox10";
+        textBox10.Size = new Size(39, 27);
+        textBox10.TabIndex = 93;
+        // 
+        // textBox11
+        // 
+        textBox11.Location = new Point(473, 90);
+        textBox11.Name = "textBox11";
+        textBox11.Size = new Size(138, 27);
+        textBox11.TabIndex = 92;
+        // 
+        // textBox2
+        // 
+        textBox2.Location = new Point(83, 86);
+        textBox2.Name = "textBox2";
+        textBox2.Size = new Size(27, 27);
+        textBox2.TabIndex = 91;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(48, 89);
+        label1.Name = "label1";
+        label1.Size = new Size(29, 20);
+        label1.TabIndex = 90;
+        label1.Text = "Số:";
+        // 
+        // label12
+        // 
+        label12.AutoSize = true;
+        label12.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+        label12.Location = new Point(570, 49);
+        label12.Name = "label12";
+        label12.Size = new Size(281, 28);
+        label12.TabIndex = 89;
+        label12.Text = "Độc lập - Tự do - Hạnh phúc";
+        // 
+        // label11
+        // 
+        label11.AutoSize = true;
+        label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+        label11.Location = new Point(501, 22);
+        label11.Name = "label11";
+        label11.Size = new Size(415, 28);
+        label11.TabIndex = 88;
+        label11.Text = "CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM";
+        // 
+        // textBox1
+        // 
+        textBox1.Location = new Point(23, 26);
+        textBox1.Name = "textBox1";
+        textBox1.PlaceholderText = "Tên chủ dự án ";
+        textBox1.Size = new Size(326, 27);
+        textBox1.TabIndex = 87;
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(116, 90);
+        label2.Name = "label2";
+        label2.Size = new Size(15, 20);
+        label2.TabIndex = 100;
+        label2.Text = "/";
+        // 
+        // textBox3
+        // 
+        textBox3.Location = new Point(137, 87);
+        textBox3.Name = "textBox3";
+        textBox3.Size = new Size(33, 27);
+        textBox3.TabIndex = 99;
+        // 
+        // textBox4
+        // 
+        textBox4.Location = new Point(319, 213);
+        textBox4.Name = "textBox4";
+        textBox4.PlaceholderText = "Tên dự án";
+        textBox4.Size = new Size(326, 27);
+        textBox4.TabIndex = 101;
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+        label3.Location = new Point(186, 169);
+        label3.Name = "label3";
+        label3.Size = new Size(662, 41);
+        label3.TabIndex = 102;
+        label3.Text = "HỒ SƠ TRẢ KẾ HOẠCH BẢO VỆ MÔI TRƯỜNG";
+        // 
+        // textBox16
+        // 
+        textBox16.Location = new Point(656, 411);
+        textBox16.Name = "textBox16";
+        textBox16.Size = new Size(295, 27);
+        textBox16.TabIndex = 115;
+        // 
+        // textBox15
+        // 
+        textBox15.Location = new Point(332, 411);
+        textBox15.Name = "textBox15";
+        textBox15.Size = new Size(266, 27);
+        textBox15.TabIndex = 114;
+        // 
+        // label19
+        // 
+        label19.AutoSize = true;
+        label19.Location = new Point(296, 414);
+        label19.Name = "label19";
+        label19.Size = new Size(30, 20);
+        label19.TabIndex = 113;
+        label19.Text = "Fax";
+        // 
+        // label18
+        // 
+        label18.AutoSize = true;
+        label18.Location = new Point(604, 414);
+        label18.Name = "label18";
+        label18.Size = new Size(46, 20);
+        label18.TabIndex = 112;
+        label18.Text = "Email";
+        // 
+        // textBox14
+        // 
+        textBox14.Location = new Point(286, 374);
+        textBox14.Name = "textBox14";
+        textBox14.Size = new Size(665, 27);
+        textBox14.TabIndex = 111;
+        // 
+        // label17
+        // 
+        label17.Location = new Point(11, 374);
+        label17.Name = "label17";
+        label17.Size = new Size(279, 27);
+        label17.TabIndex = 110;
+        label17.Text = "- Người đại diện theo pháp luật của (1):\r\n\r\n";
+        // 
+        // textBox13
+        // 
+        textBox13.Location = new Point(348, 341);
+        textBox13.Name = "textBox13";
+        textBox13.Size = new Size(603, 27);
+        textBox13.TabIndex = 109;
+        // 
+        // label5
+        // 
+        label5.AutoSize = true;
+        label5.Location = new Point(11, 341);
+        label5.Name = "label5";
+        label5.Size = new Size(331, 20);
+        label5.TabIndex = 108;
+        label5.Text = "- Giấy chứng nhận đầu tư đăng ký kinh doanh số:";
+        // 
+        // label16
+        // 
+        label16.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+        label16.Location = new Point(11, 250);
+        label16.Name = "label16";
+        label16.Size = new Size(943, 44);
+        label16.TabIndex = 107;
+        label16.Text = resources.GetString("label16.Text");
+        // 
+        // textBox5
+        // 
+        textBox5.Location = new Point(95, 411);
+        textBox5.Name = "textBox5";
+        textBox5.Size = new Size(195, 27);
+        textBox5.TabIndex = 106;
+        // 
+        // label7
+        // 
+        label7.AutoSize = true;
+        label7.Location = new Point(11, 414);
+        label7.Name = "label7";
+        label7.Size = new Size(78, 20);
+        label7.TabIndex = 105;
+        label7.Text = "Điện thoại";
+        // 
+        // textBox6
+        // 
+        textBox6.Location = new Point(214, 308);
+        textBox6.Name = "textBox6";
+        textBox6.Size = new Size(737, 27);
+        textBox6.TabIndex = 104;
+        // 
+        // label4
+        // 
+        label4.AutoSize = true;
+        label4.Location = new Point(11, 308);
+        label4.Name = "label4";
+        label4.Size = new Size(197, 20);
+        label4.TabIndex = 103;
+        label4.Text = "- Địa chỉ trụ sở chính của (1):";
+        // 
+        // label20
+        // 
+        label20.AutoSize = true;
+        label20.Location = new Point(11, 531);
+        label20.Name = "label20";
+        label20.Size = new Size(522, 20);
+        label20.TabIndex = 121;
+        label20.Text = "- Quy mô; công suất; công nghệ và loại hình sản xuất của dự án đầu tư, cơ sở: \r\n";
+        // 
+        // textBox7
+        // 
+        textBox7.Location = new Point(539, 528);
+        textBox7.Name = "textBox7";
+        textBox7.Size = new Size(412, 27);
+        textBox7.TabIndex = 120;
+        // 
+        // label6
+        // 
+        label6.AutoSize = true;
+        label6.Location = new Point(11, 501);
+        label6.Name = "label6";
+        label6.Size = new Size(574, 20);
+        label6.TabIndex = 119;
+        label6.Text = "- Địa điểm thực hiện dự án đầu tư, cơ sở nguồn vốn và tiến độ thực hiện dự án đầu tư\r\n";
+        // 
+        // label10
+        // 
+        label10.AutoSize = true;
+        label10.Location = new Point(11, 478);
+        label10.Name = "label10";
+        label10.Size = new Size(198, 20);
+        label10.TabIndex = 118;
+        label10.Text = "- Tên dự án đầu tư, cơ sở: (2)";
+        // 
+        // textBox12
+        // 
+        textBox12.Location = new Point(591, 498);
+        textBox12.Name = "textBox12";
+        textBox12.Size = new Size(360, 27);
+        textBox12.TabIndex = 117;
+        // 
+        // label9
+        // 
+        label9.AutoSize = true;
+        label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+        label9.Location = new Point(11, 450);
+        label9.Name = "label9";
+        label9.Size = new Size(303, 20);
+        label9.TabIndex = 116;
+        label9.Text = "1. Thông tin chung về dự án đầu tư, cơ sở:";
+        // 
+        // label8
+        // 
+        label8.AutoSize = true;
+        label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+        label8.Location = new Point(11, 561);
+        label8.Name = "label8";
+        label8.Size = new Size(113, 20);
+        label8.TabIndex = 122;
+        label8.Text = "Lý do trả hồ sơ";
+        // 
+        // richTextBox1
+        // 
+        richTextBox1.Location = new Point(12, 584);
+        richTextBox1.Name = "richTextBox1";
+        richTextBox1.Size = new Size(939, 152);
+        richTextBox1.TabIndex = 123;
+        richTextBox1.Text = "";
+        // 
+        // button2
+        // 
+        button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+        button2.Location = new Point(578, 939);
+        button2.Name = "button2";
+        button2.Size = new Size(193, 51);
+        button2.TabIndex = 127;
+        button2.Text = "Tải lên họ và tên";
+        button2.UseVisualStyleBackColor = true;
+        // 
+        // button1
+        // 
+        button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+        button1.Location = new Point(578, 825);
+        button1.Name = "button1";
+        button1.Size = new Size(193, 58);
+        button1.TabIndex = 128;
+        button1.Text = "Tải lên chữ ký";
+        button1.UseVisualStyleBackColor = true;
+        // 
+        // label43
+        // 
+        label43.AutoSize = true;
+        label43.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+        label43.Location = new Point(402, 783);
+        label43.Name = "label43";
+        label43.Size = new Size(554, 23);
+        label43.TabIndex = 126;
+        label43.Text = "(Chữ ký của người có thẩm quyền, dấu/chữ ký số của cơ quan, tổ chức)";
+        // 
+        // label42
+        // 
+        label42.AutoSize = true;
+        label42.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+        label42.Location = new Point(499, 755);
+        label42.Name = "label42";
+        label42.Size = new Size(381, 28);
+        label42.TabIndex = 125;
+        label42.Text = "QUYỀN HẠN, CHỨC VỤ CỦA NGƯỜI KÝ";
+        // 
+        // label46
+        // 
+        label46.AutoSize = true;
+        label46.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+        label46.Location = new Point(639, 909);
+        label46.Name = "label46";
+        label46.Size = new Size(76, 20);
+        label46.TabIndex = 124;
+        label46.Text = "Họ và tên";
+        // 
+        // HST_KHBVMT
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        AutoScroll = true;
+        ClientSize = new Size(984, 920);
+        Controls.Add(button2);
+        Controls.Add(button1);
+        Controls.Add(label43);
+        Controls.Add(label42);
+        Controls.Add(label46);
+        Controls.Add(richTextBox1);
+        Controls.Add(label8);
+        Controls.Add(label20);
+        Controls.Add(textBox7);
+        Controls.Add(label6);
+        Controls.Add(label10);
+        Controls.Add(textBox12);
+        Controls.Add(label9);
+        Controls.Add(textBox16);
+        Controls.Add(textBox15);
+        Controls.Add(label19);
+        Controls.Add(label18);
+        Controls.Add(textBox14);
+        Controls.Add(label17);
+        Controls.Add(textBox13);
+        Controls.Add(label5);
+        Controls.Add(label16);
+        Controls.Add(textBox5);
+        Controls.Add(label7);
+        Controls.Add(textBox6);
+        Controls.Add(label4);
+        Controls.Add(label3);
+        Controls.Add(textBox4);
+        Controls.Add(label2);
+        Controls.Add(textBox3);
+        Controls.Add(label21);
+        Controls.Add(label22);
+        Controls.Add(label23);
+        Controls.Add(textBox8);
+        Controls.Add(textBox9);
+        Controls.Add(textBox10);
+        Controls.Add(textBox11);
+        Controls.Add(textBox2);
+        Controls.Add(label1);
+        Controls.Add(label12);
+        Controls.Add(label11);
+        Controls.Add(textBox1);
+        Name = "HST_KHBVMT";
+        Text = "HST_KHBVMT";
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Label label21;
+    private Label label22;
+    private Label label23;
+    private TextBox textBox8;
+    private TextBox textBox9;
+    private TextBox textBox10;
+    private TextBox textBox11;
+    private TextBox textBox2;
+    private Label label1;
+    private Label label12;
+    private Label label11;
+    private TextBox textBox1;
+    private Label label2;
+    private TextBox textBox3;
+    private TextBox textBox4;
+    private Label label3;
+    private TextBox textBox16;
+    private TextBox textBox15;
+    private Label label19;
+    private Label label18;
+    private TextBox textBox14;
+    private Label label17;
+    private TextBox textBox13;
+    private Label label5;
+    private Label label16;
+    private TextBox textBox5;
+    private Label label7;
+    private TextBox textBox6;
+    private Label label4;
+    private Label label20;
+    private TextBox textBox7;
+    private Label label6;
+    private Label label10;
+    private TextBox textBox12;
+    private Label label9;
+    private Label label8;
+    private RichTextBox richTextBox1;
+    private Button button2;
+    private Button button1;
+    private Label label43;
+    private Label label42;
+    private Label label46;
+}
